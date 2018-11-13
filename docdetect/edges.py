4d3607d4f05e6bb6fe5e8a45f2947c9b079be498
Alessandro Zamberletti <alessandro.zamberletti@gmail.com>
2018-11-13 23:01:53 +0100

    * extracted preprocess

--- docdetect/edges.py
+++ docdetect/edges.py
@@ -3,9 +3,7 @@ import cv2
 
 
 def detect_edges(im, blur=False, blur_radius=9, thr1=100, thr2=200, remove_text=True):
-    saturation = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)[:, :, 2]
-    if blur:
-        saturation = cv2.medianBlur(saturation, blur_radius)
+    saturation = preprocess(im, blur, blur_radius)
     edges = cv2.Canny(saturation, thr1, thr2)
     if remove_text:
         characters = find_text_regions(im)
@@ -21,3 +19,10 @@ def find_text_regions(im):
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     characters, _ = mser.detectRegions(gray)
     return characters
+
+
+def preprocess(im, blur, blur_radius):
+    saturation = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)[:, :, 2]
+    if blur:
+        saturation = cv2.medianBlur(saturation, blur_radius)
+    return saturation
