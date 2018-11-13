df2085e7b240ecefc0fc04bda5d2f31b8ab69f14
Alessandro Zamberletti <alessandro.zamberletti@gmail.com>
2018-11-13 22:57:12 +0100

    * migrated canny + blur

--- docdetect/edges.py
+++ docdetect/edges.py
@@ -1,1 +1,10 @@
 # -*- coding: utf-8 -*-
+import cv2
+
+
+def detect_edges(im, blur=9, thr1=100, thr2=200):
+    saturation = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)[:, :, 2]
+    saturation = cv2.medianBlur(saturation, blur)
+    edges = cv2.Canny(saturation, thr1, thr2)
+    return edges
+
