382daa9a0f048cdcdbf3109f85b585e9b6b137f9
Alessandro Zamberletti <alessandro.zamberletti@gmail.com>
2018-11-14 14:47:04 +0100

    * refactoring

--- docdetect/edge_utils.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from cv2 import Canny, cvtColor, COLOR_BGR2HSV, COLOR_BGR2GRAY, medianBlur
-
-
-def detect_edges(im):
-    saturation = cvtColor(im, COLOR_BGR2HSV)[:, :, 2]
-    saturation = medianBlur(saturation, 9)
-    return Canny(saturation, 100, 200)
