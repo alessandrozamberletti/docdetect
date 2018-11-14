2f4cba3d3260b5a95c6be87f63b329bd9eba4cbb
Alessandro Zamberletti <alessandro.zamberletti@gmail.com>
2018-11-14 20:45:02 +0100

    * refactoring

--- docdetect/canny_edges.py
+++ docdetect/canny_edges.py
@@ -7,8 +7,9 @@ def detect_edges(im, blur=False, blur_radius=9, thr1=100, thr2=200, remove_text=
     edges = cv2.Canny(enhanced_im, thr1, thr2)
     if remove_text:
         # find characters in im
-        characters = find_characters(im)
-        # delete characters from edges mask
+        height, width = im.shape[:2]
+        characters = find_characters(im, int((width * height) / 1e2))
+        # remove characters from edges mask
         for character in characters:
             x = character[:, 1]
             y = character[:, 0]
@@ -16,10 +17,8 @@ def detect_edges(im, blur=False, blur_radius=9, thr1=100, thr2=200, remove_text=
     return edges
 
 
-def find_characters(im):
-    height, width = im.shape[:2]
-    max_size = int((width * height) / 1e2)
-    mser = cv2.MSER_create(_max_area=max_size)
+def find_characters(im, max_area):
+    mser = cv2.MSER_create(_max_area=max_area)
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     characters, _ = mser.detectRegions(gray)
     return characters
