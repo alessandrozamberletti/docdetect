9d6e24d468283a14a3d76678e6ce14cd843959bd
Alessandro Zamberletti <alessandro.zamberletti@gmail.com>
2018-11-16 20:58:21 +0100

    * initial refactoring, wip

--- docdetect/corners.py
+++ docdetect/corners.py
@@ -6,24 +6,24 @@ def find_corners(lines, im):
     height, width = im.shape[:2]
     corners = []
     vertex_id = 0
-    for idx_line1, x in enumerate(lines):
-        rho1, theta1 = x[0]
-        for idx_line2, y in enumerate(lines):
-            rho2, theta2 = y[0]
-            if lines_are_same(x, y):
+    for line1 in lines:
+        rho1, theta1 = line1
+        for line2 in lines:
+            rho2, theta2 = line2
+            if lines_are_same(line1, line2):
                 continue
             a = np.array([[np.cos(theta1), np.sin(theta1)], [np.cos(theta2), np.sin(theta2)]])
             b = np.array([[rho1], [rho2]])
             try:
                 # aX = b, solve for x
-                x0, y0 = np.linalg.solve(a, b)
-                x0, y0 = int(np.round(x0)), int(np.round(y0))
+                x0, y0 = np.round(np.linalg.solve(a, b))
+                x0, y0 = int(x0), int(y0)
 
                 angle = np.abs(theta1 - theta2) * 180 / np.pi
                 if angle < 45 or x0 < 0 or y0 < 0 or x0 > width or y0 > height:
                     continue
                 if not already_present(x0, y0, corners):
-                    corners.append([vertex_id, idx_line1, idx_line2, x, y, x0, y0])
+                    corners.append([vertex_id, line1, line2, x0, y0])
                     vertex_id += 1
             except np.linalg.linalg.LinAlgError:
                 pass
@@ -40,6 +40,4 @@ def already_present(_x0, _y0, angles):
 
 
 def lines_are_same(line1, line2):
-    rho1, theta1 = line1[0]
-    rho2, theta2 = line2[0]
-    return rho1 == rho2 and theta1 == theta2
+    return line1 == line2
