0b33d1e7c02eac24ddd01cb7f324d6792a27bcd4
Alessandro Zamberletti <alessandro.zamberletti@gmail.com>
2018-11-24 16:41:53 +0100

    * refactoring

--- docdetect/corners.py
+++ docdetect/corners.py
@@ -14,29 +14,30 @@ def find_corners(lines, im, angle_thr=45):
             angle = np.abs(theta1 - theta2) * 180 / np.pi
             if angle < angle_thr:
                 continue
-            x0, y0 = _find_intersection_coordinates(line1, line2)
-            if x0 < 0 or y0 < 0 or x0 > width or y0 > height:
+            x, y = _find_intersection_coordinates(rho1, theta1, rho2, theta2)
+            if _coordinates_are_valid(x, y, width, height):
                 continue
-            if not already_present(x0, y0, corners):
-                corners.append([vertex_id, line1, line2, x0, y0])
+            # TODO use set instead
+            if not already_present(x, y, corners):
+                corners.append([vertex_id, line1, line2, x, y])
                 vertex_id += 1
     return corners
 
 
-def _find_intersection_coordinates(line1, line2):
-    rho1, theta1 = line1
-    rho2, theta2 = line2
+def _find_intersection_coordinates(rho1, theta1, rho2, theta2):
     a = np.array([[np.cos(theta1), np.sin(theta1)], [np.cos(theta2), np.sin(theta2)]])
     b = np.array([[rho1], [rho2]])
     try:
-        # aX = b -> solve for x, see:
-        # https://stackoverflow.com/questions/46565975/find-intersection-point-of-two-lines-drawn-using-houghlines-opencv
-        x0, y0 = np.round(np.linalg.solve(a, b))
-        return int(x0), int(y0)
+        # aX = b, solve for x
+        x, y = np.round(np.linalg.solve(a, b))
+        return int(x), int(y)
     except np.linalg.linalg.LinAlgError:
         # singular matrix
-        pass
-    return -1, -1
+        return -1, -1
+
+
+def _coordinates_are_valid(x, y, width, height):
+    return x < 0 or y < 0 or x > width or y > height
 
 
 # TODO _x0, _y0 becomes line, if becomes lines_are_same
