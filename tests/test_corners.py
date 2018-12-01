68ce902929cee0ed2ff3d91e1b46337e9aa0ad0f
Alessandro Zamberletti <alessandro.zamberletti@gmail.com>
2018-12-01 21:52:45 +0100

    * added tests

--- tests/test_corners.py
+++ tests/test_corners.py
@@ -1,10 +1,22 @@
 # -*- coding: utf-8 -*-
 from unittest import TestCase, main
 
+from docdetect.corners import _angle_is_valid, _coordinates_are_valid
+
 
 class TestCorners(TestCase):
-    def setUp(self):
-        pass
+    def test_angle_is_valid(self):
+        self.assertEqual(True, _angle_is_valid((0, 0.26), (0, 0.08), 10))
+        self.assertEqual(False, _angle_is_valid((0, 0.26), (0, 0.08), 11))
+
+    def test_coordinates_are_valid(self):
+        self.assertEqual(True, _coordinates_are_valid(1, 1, 10, 10))
+        self.assertEqual(False, _coordinates_are_valid(-1, 1, 10, 10))
+        self.assertEqual(False, _coordinates_are_valid(1, -1, 10, 10))
+        self.assertEqual(False, _coordinates_are_valid(10, 1, 10, 10))
+        self.assertEqual(False, _coordinates_are_valid(1, 10, 10, 10))
+        self.assertEqual(False, _coordinates_are_valid(11, 1, 10, 10))
+        self.assertEqual(False, _coordinates_are_valid(1, 11, 10, 10))
 
 
 if __name__ == "__main__":
