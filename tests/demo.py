35e401d10b18f22900f3be02a3534ac0917e8bd0
Alessandro Zamberletti <alessandro.zamberletti@gmail.com>
2018-12-06 19:34:10 +0100

    * now showing only biggest rect

--- tests/demo.py
+++ tests/demo.py
@@ -11,20 +11,32 @@ video = cv2.VideoCapture(video_path)
 while video.isOpened():
     ret, frame = video.read()
     if ret:
-        # frame = cv2.bitwise_not(frame)
-        edges = docdetect.detect_edges(frame, thr1=50, thr2=100)
+        frame = cv2.bitwise_not(frame)
+        edges = docdetect.detect_edges(frame)
         lines_unique = docdetect.detect_lines(edges)
         _intersections = docdetect.find_intersections(lines_unique, frame)
         rects = docdetect.find_quadrilaterals(_intersections)
 
+        area = -1
+        best = None
         for rect in rects:
-            cv2.line(frame, rect[0], rect[1], (0, 255, 0), thickness=5, lineType=8)
-            cv2.line(frame, rect[1], rect[2], (0, 255, 0), thickness=5, lineType=8)
-            cv2.line(frame, rect[2], rect[3], (0, 255, 0), thickness=5, lineType=8)
-            cv2.line(frame, rect[3], rect[0], (0, 255, 0), thickness=5, lineType=8)
+            points = (rect[0], rect[1], rect[2], rect[3])
+            x, y = zip(*points)
+            width = max(x) - min(x)
+            height = max(y) - min(y)
+            if width*height > area:
+                best = rect
+                area = width*height
+
+        if best is not None:
+            cv2.line(frame, best[0], best[1], (255, 0, 0), thickness=5, lineType=8)
+            cv2.line(frame, best[1], best[2], (255, 0, 0), thickness=5, lineType=8)
+            cv2.line(frame, best[2], best[3], (255, 0, 0), thickness=5, lineType=8)
+            cv2.line(frame, best[3], best[0], (255, 0, 0), thickness=5, lineType=8)
+
         cv2.startWindowThread()
         cv2.namedWindow('output')
         cv2.moveWindow('output', 500, 30)
         cv2.imshow('output', frame)
-        cv2.waitKey(10)
+        cv2.waitKey(1)
 video.release()
